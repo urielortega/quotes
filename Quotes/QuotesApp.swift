706ab8603ec4055b677f9efe72146d5690f1ff2d
Uriel Ortega <urielortega2509@gmail.com>
2024-08-14 16:47:34 -0600

    Initial Commit

---
+++ Quotes/QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesApp.swift
+//  Quotes
+//
+//  Created by Uriel Ortega on 14/08/24.
+//
+
+import SwiftUI
+
+@main
+struct QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
