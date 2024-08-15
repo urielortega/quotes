5bd0a31a71588f2e62de957d0ac1c6a3614976c3
Uriel Ortega <urielortega2509@gmail.com>
2024-08-14 18:14:34 -0600

    Created testJSONDecoderDecodesQuote function.

--- QuotesTests/QuotesTests.swift
+++ QuotesTests/QuotesTests.swift
@@ -9,5 +9,16 @@ import XCTest
 @testable import Quotes
 
 class QuotesTests: XCTestCase {
+    func testJSONDecoderDecodesQuote() throws {
+        let decoder = JSONDecoder()
 
+        // The decoder checks that the types in the JSON match the types expected by the Quote struct. If any required fields are missing or of the wrong type, the decoder throws an error:
+        let quote = try decoder.decode(Quote.self, from: testQuote)
+
+        XCTAssertEqual(quote.author, "Albert Einstein")
+
+        let expectedText = "If You Are Out To Describe The Truth, Leave Elegance To The Tailor."
+
+        XCTAssertEqual(quote.text, expectedText)
+    }
 }
